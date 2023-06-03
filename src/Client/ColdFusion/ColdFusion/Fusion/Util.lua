c95f4a353788b457123ad809fbd5c44a0d75f8ee
Vijay Brara <Vijaybrara@gmail.com>
2023-06-04 03:29:37 +0700

    Changes

--- src/Client/ColdFusion/ColdFusion/Fusion/Util.lua
+++ src/Client/ColdFusion/ColdFusion/Fusion/Util.lua
@@ -53,7 +53,6 @@ type FusionSpecialKey = _Types.FusionSpecialKey
 type FusionKey = _Types.FusionKey
 type FusionPropertyTable = _Types.FusionPropertyTable
 
-
 return function(interface: any)
 	local Util = {}
 	Util.__index = Util
